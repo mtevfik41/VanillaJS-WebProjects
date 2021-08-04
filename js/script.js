52574ed7e0f43ff84ecf138c56cf17d4e6543fd6
unknown <mtevfik41@gmail.com>
2021-08-05 01:33:57 +0300

    UI has been designed to access all projects.

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// const box = document.getElementsByClassName('box');
+// for (let i = 0; i < box.length; i++) {
+//     box[i].addEventListener('mouseover', () => {
+//         const span = document.getElementsByTagName('span');
+//         for (let j = 0; j < span.length; j++) {
+//             if (i == j) {
+//                 span[j].style.display = 'inline-block';
+
+//             }
+//         }
+//     });
+//     box[i].addEventListener('mouseout', () => {
+//         const span = document.getElementsByTagName('span');
+//         for (let j = 0; j < span.length; j++) {
+//             if (i == j) {
+//                 span[j].style.display = 'none';
+//             }
+//         }
+//     });
+// }
